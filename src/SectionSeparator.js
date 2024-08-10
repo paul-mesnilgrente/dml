bbb0a79212513d6ac2547365f929c0f02354d01f
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-08-10 18:26:09 +0200

    fix: made the separator always show full text

--- src/SectionSeparator.js
+++ src/SectionSeparator.js
@@ -1,10 +1,10 @@
 export default function SectionSeparator({ className, text, author }) {
   return (
-    <section className={`container-fluid section-separator ${className}`}>
-      <div className={`row align-items-center`}>
+    <section className={`container-fluid separator ${className}`}>
+      <div className="row separator__container align-items-center">
         <figure className="col black-filter">
-          <blockquote className="blockquoteote">"{text}"</blockquote>
-          <figcaption>{author}</figcaption>
+          <blockquote className="display-5 fw-bold">"{text}"</blockquote>
+          <figcaption className="fs-4">{author}</figcaption>
         </figure>
       </div>
     </section>
