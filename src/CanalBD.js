e6e99d5c6b3faf302e8fb16093a8119de56cda38
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-08-10 19:05:05 +0200

    feat: restyled CanalBD

--- src/CanalBD.js
+++ src/CanalBD.js
@@ -3,23 +3,27 @@ import CanalBDImage from "./images/supporters/canal_bd.png";
 export default function CanalBD() {
   return (
     <section className="container">
-      <h1>Nos livres sur CanalBD</h1>
+      <div className="row align-items-center">
+        <div className="col-12 col-md-6">
+          <h1>Nos livres sur CanalBD</h1>
 
-      <ul>
-        <li>Aller sur CanalBD</li>
-        <li>Sélectionnez vos livres</li>
-        <li>La librairie vous livre</li>
-      </ul>
+          <ul class="fs-4 list-unstyled">
+            <li>Commande en ligne</li>
+            <li>Retrait en librairie</li>
+            <li>La librairie vous livre</li>
+          </ul>
+        </div>
 
-      <div className="text-center">
-        <a
-          href="https://www.canalbd.net/dans-ma-librairie"
-          target="_blank"
-          rel="noreferrer"
-          alt="Dans ma librairie sur CanalBD"
-        >
-          <img src={CanalBDImage} alt="CanalBD" />
-        </a>
+        <div className="col-12 col-md-6 text-center">
+          <a
+            href="https://www.canalbd.net/dans-ma-librairie"
+            target="_blank"
+            rel="noreferrer"
+            alt="Dans ma librairie sur CanalBD"
+          >
+            <img src={CanalBDImage} alt="CanalBD" />
+          </a>
+        </div>
       </div>
     </section>
   );
