39389ba4dcc43b84690da0a82c23ec8df11a927e
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-08-10 18:05:46 +0200

    refactor: made a simpler history style

--- src/History.js
+++ src/History.js
@@ -1,33 +1,24 @@
 export default function History() {
+  const events = [
+    { date: "-9000 avant J.-C.", description: "Découverte de l'écriture" },
+    { date: "1454", description: "Invention de l'imprimerie par Gutenberg" },
+    { date: "1997", description: 'Création de "BD Bulle"' },
+    { date: "2009", description: "Découverte de l'écriture" },
+    { date: "2021", description: "Rachat par Grégory et Julie" },
+  ];
   return (
     <section className="container" id="histoire">
       <h1>Notre histoire</h1>
 
       <ol className="list-group mt-3">
-        <li className="list-group-item d-flex justify-content-between align-items-start">
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">-9000 avant J.-C.</div>
-            Découverte de l'écriture
-          </div>
-        </li>
-        <li className="list-group-item d-flex justify-content-between align-items-start">
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">1454</div>
-            Invention de l'imprimerie par Gutenberg
-          </div>
-        </li>
-        <li className="list-group-item d-flex justify-content-between align-items-start">
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">1982</div>
-            Création de "BD Bulle"
-          </div>
-        </li>
-        <li className="list-group-item d-flex justify-content-between align-items-start">
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">2011</div>
-            Changement de nom "Dans ma librairie"
-          </div>
-        </li>
+        {events.map((event) => (
+          <li className="list-group-item d-flex justify-content-between align-items-start">
+            <div className="ms-2 me-auto">
+              <div className="fw-bold">{event.date}</div>
+              {event.description}
+            </div>
+          </li>
+        ))}
       </ol>
     </section>
   );
