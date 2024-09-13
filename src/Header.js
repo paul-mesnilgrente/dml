99448ebd57b9268985c8768978b4871f259c241f
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-09-13 20:46:32 +0200

    wip(header): added the bubble...
    the responsive side isn't done yet

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,12 @@ function Header() {
       <div className="container">
         <div className="row align-items-center">
           <div className="col">
-            <h1 className="text-center">Dans ma librairie</h1>
+            <div className="header__title-container text-center">
+              <h1 className="header__title">Dans ma librairie</h1>
+              <p className="header__subtitle h2">
+                Librairie ENFANTS/ADOS, BD et Manga
+              </p>
+            </div>
           </div>
         </div>
       </div>
