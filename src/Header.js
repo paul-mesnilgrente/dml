c8f8d5815a705bf744633c0f9f15ef2da0fe49fd
Paul Mesnilgrente <web@paul-mesnilgrente.com>
2024-09-13 21:13:42 +0200

    feat(header): made it responsive

--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,11 @@
 function Header() {
   return (
     <header className="header">
-      <div className="container">
-        <div className="row align-items-center">
-          <div className="col">
-            <div className="header__title-container text-center">
-              <h1 className="header__title">Dans ma librairie</h1>
-              <p className="header__subtitle h2">
-                Librairie ENFANTS/ADOS, BD et Manga
-              </p>
-            </div>
-          </div>
-        </div>
+      <div className="header__title-container text-center">
+        <h1 className="header__title">Dans ma librairie</h1>
+        <p className="header__subtitle h2">
+          Librairie ENFANTS/ADOS, BD et Manga
+        </p>
       </div>
     </header>
   );
